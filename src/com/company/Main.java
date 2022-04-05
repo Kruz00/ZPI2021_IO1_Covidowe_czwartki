1a58760199504cfffc4a2259a078b68a3e0c3188
Rozycki, Bartosz <bartek.rozycki@icloud.com>
2022-04-05 12:59:28 +0200

    fix: one line printing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Covidowe czwartki" + " bartekrozycki" + " Scrum Master");
-        System.out.println("Kruz00" + " Tester");
-        System.out.println("JakZaj" + " developer");
-        System.out.println("qushishu" + " tester");
-        System.out.println("jchrustowski" + " developer");
-
+        System.out.println("Covidowe czwartki  bartekrozycki  Scrum Master\nKruz00 Tester\nJakZaj developer\nqushishu tester\njchrustowski developer");
     } 
 }
