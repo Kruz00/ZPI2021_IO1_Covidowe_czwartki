93eedd360bd6fd6624262b24285dbeeac307e9b4
Rozycki, Bartosz <bartek.rozycki@icloud.com>
2022-04-05 19:57:53 +0200

    cleanup

--- src/com/company/Main.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Covidowe czwartki  bartekrozycki  Scrum Master\nKruz00 Tester\nJakZaj developer\nqushishu tester\njchrustowski developer");
-    } 
-}
--- src/com/company/arithmetics/IArithmeticsDiff.java
+++
@@ -1,5 +0,0 @@
-package com.company.arithmetics;
-
-public interface IArithmeticsDiff {
-    double Difference(double A, double B);
-}
