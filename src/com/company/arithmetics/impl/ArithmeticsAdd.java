60b2ae641b215ff0598c83b47ff007789a19b24f
Bartosz Różycki <bartek.rozycki@icloud.com>
2022-04-06 08:47:09 +0200

    Revert "rofl"

--- src/com/company/arithmetics/impl/ArithmeticsAdd.java
+++ src/com/company/arithmetics/impl/ArithmeticsAdd.java
@@ -4,7 +4,7 @@ import com.company.arithmetics.IArithmeticsAdd;
 
 public class ArithmeticsAdd implements IArithmeticsAdd {
 
-	@Override // rofl
+	@Override
 	public Double Addition(Double A, Double B) {
 		return A + B;
 	}
