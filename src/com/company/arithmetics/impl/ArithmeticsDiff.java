573ebdc2557d32f2581363404bd2abf96537c298
qushishu <krzysiek22.00@o2.pl>
2022-04-06 08:52:43 +0200

    ulala

--- src/com/company/arithmetics/impl/ArithmeticsDiff.java
+++ src/com/company/arithmetics/impl/ArithmeticsDiff.java
@@ -3,7 +3,7 @@ package com.company.arithmetics.impl;
 import com.company.arithmetics.IArithmeticsDiff;
 
 public class ArithmeticsDiff implements IArithmeticsDiff {
-
+    //ulala
     @Override
     public double Difference(double A, double B) {
         return A - B;
