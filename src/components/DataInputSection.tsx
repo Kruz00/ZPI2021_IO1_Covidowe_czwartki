edbe4b15c7f7cf54868abf728f4d7b9b3ddcca5f
jchrustowski <bolek9lodz99@gmail.com>
2022-09-12 19:33:07 +0200

    Stworzono podstawową makietę sekcji wprowadzania danych (potrzebna logika, ew. stylowanie)

--- src/components/DataInputSection.tsx
+++ src/components/DataInputSection.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DropdownList from "react-widgets/DropdownList";
+import "react-widgets/styles.css";
 
 const DataInputSection = () => {
 
+  const currency = ["EUR", "PLN", "USD", "GBP"]
+  const periods = ["Tydzień", "2 tygodnie", "Miesiąc", "Kwartał", "Pół roku", "Rok"]
+  const [mainCurrency, setMainCurrency] = useState("")
+  const [secondCurrency, setSecondCurrency] = useState("")
+  const [period, setPeriod] = useState("")
   return (
     <div>
-      Test
+      <div> Waluta główna:</div>
+      <DropdownList onChange={value => setMainCurrency(value)} data={currency}/>
+      <div> Waluta dla kursu:</div>
+      <DropdownList onChange={value => setSecondCurrency(value)} data={currency}/>
+      <div> Przedział czasowy:</div>
+      <DropdownList onChange={value => setPeriod(value)} data={periods}/>
+      <button>Analizuj</button>
     </div>
   )
 }
