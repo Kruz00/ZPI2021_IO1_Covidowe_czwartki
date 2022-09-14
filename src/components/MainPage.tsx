a3c98b4d00721ba73c076c72d6acad31ec15c2fc
jchrustowski <bolek9lodz99@gmail.com>
2022-09-14 21:44:46 +0200

    Dodano obsługę błędów z API. Dodano warunkową widoczność sekcji analizy

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import DataInputSection from './DataInputSection'
 import AnalysisSection from './AnalysisSection'
+import {getTimeIntervalJSON} from '../API NBP/ImplementApiNBP'
 
 const MainPage = () => {
   const currency = ["EUR", "PLN", "USD", "GBP"]
@@ -9,7 +10,9 @@ const MainPage = () => {
   const [secondCurrency, setSecondCurrency] = useState("")
   const [period, setPeriod] = useState("")
   const [currencyError, setCurrencyError] = useState(false)
-  const analize = () => {
+  const [apiError, setApiError] = useState(false)
+  const [analysisSectionVisibilty, setAnalysisSectionVisibilty] = useState(false)
+  const analize = async () => {
     if (mainCurrency === "" || secondCurrency === "") {
       return;
     }
@@ -19,14 +22,23 @@ const MainPage = () => {
     } else {
       setCurrencyError(false)
     }
+    try {
+      let response = await getTimeIntervalJSON(mainCurrency, 0)
 
+    } catch (error) {
+      setApiError(true)
+    }
+
+    setAnalysisSectionVisibilty(true)
   }
   return (
     <div>
       <DataInputSection currency={currency} periods={periods} mainCurrencyCallback={setMainCurrency}
-                        secondCurrencyCallback={setSecondCurrency} periodCallback={setPeriod} analysisButtonCallback={analize}/>
+                        secondCurrencyCallback={setSecondCurrency} periodCallback={setPeriod}
+                        analysisButtonCallback={analize}/>
       {currencyError ? <div>Wybór dwóch takich samych walut nie dozwolony</div> : null}
-      <AnalysisSection/>
+      {apiError ? <div>Problem z komunikacją z API</div> : null}
+      {analysisSectionVisibilty ? <AnalysisSection/> : null}
     </div>
 
   )
